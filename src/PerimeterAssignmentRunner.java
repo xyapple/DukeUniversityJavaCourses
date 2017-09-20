17aa775d47067149c71aecf10a5f909b8e0c5768
xyapple <ydeascentis@gmail.com>
2017-09-20 13:19:36 -0400

    Homework for week1

---
+++ src/PerimeterAssignmentRunner.java
@@ -0,0 +1,69 @@
+import edu.duke.*;
+import java.io.File;
+
+public class PerimeterAssignmentRunner {
+    public int getNumPoints (Shape s) {
+        
+        return 0;
+    }
+
+    public double getAverageLength(Shape s) {
+        // Put code here
+        return 0.0;
+    }
+
+    public double getLargestSide(Shape s) {
+        // Put code here
+        return 0.0;
+    }
+
+    public double getLargestX(Shape s) {
+        // Put code here
+        return 0.0;
+    }
+
+    public double getLargestPerimeterMultipleFiles() {
+        // Put code here
+        return 0.0;
+    }
+
+    public String getFileWithLargestPerimeter() {
+        // Put code here
+        File temp = null;    // replace this code
+        return temp.getName();
+    }
+
+    public void testPerimeterMultipleFiles() {
+        // Put code here
+    }
+
+    public void testFileWithLargestPerimeter() {
+        // Put code here
+    }
+
+    // This method creates a triangle that you can use to test your other methods
+    public void triangle(){
+        Shape triangle = new Shape();
+        triangle.addPoint(new Point(0,0));
+        triangle.addPoint(new Point(6,0));
+        triangle.addPoint(new Point(3,6));
+        for (Point p : triangle.getPoints()){
+            System.out.println(p);
+        }
+        double peri = getPerimeter(triangle);
+        System.out.println("perimeter = "+peri);
+    }
+
+    // This method prints names of all files in a chosen folder that you can use to test your other methods
+    public void printFileNames() {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            System.out.println(f);
+        }
+    }
+
+    public static void main (String[] args) {
+        PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
+        pr.testPerimeter();
+    }
+}
---
+++ src/perimeter.java
@@ -0,0 +1,2 @@
+public class perimeter {
+}
